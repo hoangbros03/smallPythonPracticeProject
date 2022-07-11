f8557ef6e07c6711f4b5f2ce3db36fef024a6bd0
hoangbros03 <87892619+hoangbros03@users.noreply.github.com>
2022-07-11 16:07:34 +0700

    Show history. Learned State in streamlit

--- Dice rolling/toRunGraphic.py
+++ Dice rolling/toRunGraphic.py
@@ -5,12 +5,15 @@
 Here's our first attempt at using data to create a table:
 """
 
+from turtle import onkeypress
 import streamlit as st
 import pandas as pd
 import numpy as np
 import random
 showResult = False
 numberResult = -1
+if 'historyOfRolling' not in st.session_state:
+    st.session_state.historyOfRolling = ""
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
 
@@ -25,15 +28,34 @@ st.write("Welcome to Dice Rolling System!")
 ReRollTimes = st.slider("Firstly, type number of re-roll times you want (default is 1): ",
                         1, 10, key="ReRollTimes")
 
-if (st.button("Roll now!")):
+
+if(st.button("Roll now!")):
     for i in range(1, ReRollTimes+1):
         numberResult = random.randint(1, 6)
     st.write("The result is: " + str(numberResult))
+    if(len(st.session_state.historyOfRolling) == 0):
+        st.session_state.historyOfRolling = st.session_state.historyOfRolling + \
+            str(numberResult)
+    else:
+        st.session_state.historyOfRolling = st.session_state.historyOfRolling + \
+            ", " + str(numberResult)
+
+
+st.write("The history of rolling is: " +
+         str(st.session_state.historyOfRolling))
 
 add_selectbox = st.sidebar.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone')
 )
+
 add_typeInforLabel = st.sidebar.text_input(
     label="Type your contact information here!")
+if(st.sidebar.button("Submit my information")):
+    if(add_typeInforLabel):
+        st.sidebar.write("Thanks for submitting!")
+    else:
+        st.sidebar.write("Please input your information!")
+
+
 # Add a slider to the sidebar:
